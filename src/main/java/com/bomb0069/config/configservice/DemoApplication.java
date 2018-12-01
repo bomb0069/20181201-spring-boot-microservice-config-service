919e1a1112dfc711ee4152b3bdc29165c3510578
Karan Sivrat <bomb0069@gmail.com>
2018-12-01 15:53:21 +0700

    initial spring-boot config-service

---
+++ src/main/java/com/bomb0069/config/configservice/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.bomb0069.config.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bomb0069/config/configservice/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bomb0069.config.configservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
